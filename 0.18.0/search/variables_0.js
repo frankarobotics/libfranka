5e05f2bd62de94edd5ed4ace0c164da0d5798fcc
kmohyeldin <kmohyeldin@users.noreply.github.com>
2025-11-10 10:22:25 +0000

    deploy: c5c66bb25a987f122bb424549f5e3b283a41d2f2

---
+++ 0.18.0/search/variables_0.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['accelerometer_5fbottom_0',['accelerometer_bottom',['../structfranka_1_1RobotState.html#ab40be1e08d2fde7b50c36722cf9d70f2',1,'franka::RobotState']]],
+  ['accelerometer_5ftop_1',['accelerometer_top',['../structfranka_1_1RobotState.html#a91ae21478ca936f23b583379930111bf',1,'franka::RobotState']]],
+  ['actual_5fpower_2',['actual_power',['../structfranka_1_1VacuumGripperState.html#a4230c68698cdbf6c1c560e181133bdc3',1,'franka::VacuumGripperState']]]
+];
---
+++ 0.18.0/search/variables_10.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['self_5fcollision_5favoidance_5fviolation_0',['self_collision_avoidance_violation',['../structfranka_1_1Errors.html#adf68f6333624cb5558864441a991de8c',1,'franka::Errors']]],
+  ['server_5fversion_1',['server_version',['../structfranka_1_1IncompatibleVersionException.html#a0928098d8c32f405d17b65a0f004b5ab',1,'franka::IncompatibleVersionException']]],
+  ['start_5felbow_5fsign_5finconsistent_2',['start_elbow_sign_inconsistent',['../structfranka_1_1Errors.html#aa6de1956ac056792a1dea6b9ddd52a50',1,'franka::Errors']]]
+];
