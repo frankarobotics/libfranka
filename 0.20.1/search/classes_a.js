97bdc43ad79d634969a80e08521bc93525329435
andreafranceschetti <andreafranceschetti@users.noreply.github.com>
2026-01-16 15:11:53 +0000

    deploy: b8933f9ca22bb0a31c53876a6dcc902deba6a1ba

---
+++ 0.20.1/search/classes_a.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['positionbasedjointvelocitylimitconstants_0',['PositionBasedJointVelocityLimitConstants',['../structfranka_1_1PositionBasedJointVelocityLimitConstants.html',1,'franka']]],
+  ['protocolexception_1',['ProtocolException',['../structfranka_1_1ProtocolException.html',1,'franka']]]
+];
